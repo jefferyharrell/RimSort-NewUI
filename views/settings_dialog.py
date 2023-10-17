9834ef007675e629b9301032a7f1f0bbcc488a8b
Jeffery Harrell <jefferyharrell@gmail.com>
2023-10-17 16:04:22 -0700

    Add game location and config folder location to General page

--- views/settings_dialog.py
+++ views/settings_dialog.py
@@ -12,6 +12,9 @@ from PySide6.QtWidgets import (
     QGroupBox,
     QRadioButton,
     QPushButton,
+    QFileDialog,
+    QSpacerItem,
+    QSizePolicy,
 )
 
 from models.settings import Settings
@@ -93,6 +96,84 @@ class SettingsDialog(QDialog):
 
         page_layout.setContentsMargins(0, 0, 0, 0)
 
+        # Create a QGroupBox without a title
+        group_box = QGroupBox(page)
+
+        # Create a QVBoxLayout for the QGroupBox to hold the label and the QHBoxLayout
+        group_box_layout = QVBoxLayout(group_box)
+        group_box_layout.setSpacing(0)
+
+        # Add a label at the top of the QGroupBox
+        game_location_label = QLabel("Game Location", group_box)
+        group_box_layout.addWidget(game_location_label)
+
+        # Create a QHBoxLayout
+        hbox_layout = QHBoxLayout()
+
+        # Add a label to the left of the QHBoxLayout
+        self.game_location = QLabel(self.settings.game_location, group_box)
+        self.game_location.setWordWrap(True)
+        font_metrics = self.game_location.fontMetrics()
+        desired_line_count = 3
+        min_height = font_metrics.lineSpacing() * desired_line_count
+        self.game_location.setMinimumHeight(min_height)
+        hbox_layout.addWidget(self.game_location)
+
+        # Add a fixed-width spacer
+        desired_width = 12
+        spacer = QSpacerItem(desired_width, 0, QSizePolicy.Fixed, QSizePolicy.Minimum)
+        hbox_layout.addSpacerItem(spacer)
+
+        # Add a button to the right of the QHBoxLayout
+        game_location_choose_button = QPushButton("Choose…", group_box)
+        button_width = game_location_choose_button.sizeHint().width()
+        game_location_choose_button.setFixedWidth(button_width)
+        game_location_choose_button.clicked.connect(self._on_choose_game_location)
+        hbox_layout.addWidget(game_location_choose_button)
+
+        # Add the QHBoxLayout to the QVBoxLayout of the QGroupBox
+        group_box_layout.addLayout(hbox_layout)
+
+        # Second row: Config Folder Location
+
+        # Add a label at the top of the QGroupBox for the config folder location
+        config_folder_location_label = QLabel("Config Folder Location", group_box)
+        group_box_layout.addWidget(config_folder_location_label)
+
+        # Create another QHBoxLayout for the config folder location
+        hbox_layout_config = QHBoxLayout()
+
+        # Add a label to the left of the QHBoxLayout for the config folder location
+        self.config_folder_location = QLabel(
+            self.settings.config_folder_location, group_box
+        )
+        self.config_folder_location.setWordWrap(True)
+        font_metrics = self.config_folder_location.fontMetrics()
+        desired_line_count = 3
+        min_height = font_metrics.lineSpacing() * desired_line_count
+        self.config_folder_location.setMinimumHeight(min_height)
+        hbox_layout_config.addWidget(self.config_folder_location)
+
+        # Add a fixed-width spacer
+        desired_width = 12
+        spacer = QSpacerItem(desired_width, 0, QSizePolicy.Fixed, QSizePolicy.Minimum)
+        hbox_layout_config.addSpacerItem(spacer)
+
+        # Add another "Choose…" button to the right of the QHBoxLayout for the config folder location
+        config_folder_location_choose_button = QPushButton("Choose…", group_box)
+        button_width_config = config_folder_location_choose_button.sizeHint().width()
+        config_folder_location_choose_button.setFixedWidth(button_width_config)
+        config_folder_location_choose_button.clicked.connect(
+            self._on_choose_config_folder_location
+        )
+        hbox_layout_config.addWidget(config_folder_location_choose_button)
+
+        # Add the QHBoxLayout for the config folder location to the QVBoxLayout of the QGroupBox
+        group_box_layout.addLayout(hbox_layout_config)
+
+        # Add the QGroupBox to the main QVBoxLayout
+        page_layout.addWidget(group_box)
+
         self.stacked_widget.addWidget(page)
 
     def _do_sorting_page(self) -> None:
@@ -151,6 +232,16 @@ class SettingsDialog(QDialog):
         self.settings.save()
         self.close()
 
+    def _on_choose_game_location(self) -> None:
+        game_location, _ = QFileDialog.getOpenFileName(self)
+        if game_location != "":
+            self.settings.game_location = game_location
+
+    def _on_choose_config_folder_location(self) -> None:
+        config_folder_location = QFileDialog.getExistingDirectory(self)
+        if config_folder_location != "":
+            self.settings.config_folder_location = config_folder_location
+
     def _on_sorting_algorithm_button_toggled(self, checked: bool) -> None:
         if checked:
             if self.sender() == self.alphabetical_button:
@@ -161,6 +252,11 @@ class SettingsDialog(QDialog):
                 self.settings.sorting_algorithm = Settings.SortingAlgorithm.RADIOLOGICAL
 
     def on_settings_changed(self) -> None:
+        self.game_location.setText(self.settings.game_location)
+        self.config_folder_location.setText(self.settings.config_folder_location)
+        # self.steam_mods_folder_location.setText(self.settings.steam_mods_folder_location)
+        # self.local_mods_folder_location.setText(self.settings.local_mods_folder_location)
+
         if self.settings.sorting_algorithm == Settings.SortingAlgorithm.ALPHABETICAL:
             self.alphabetical_button.setChecked(True)
         elif self.settings.sorting_algorithm == Settings.SortingAlgorithm.TOPOLOGICAL:
