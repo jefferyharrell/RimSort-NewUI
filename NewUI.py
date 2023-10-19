498cf20a58810fa3b46c7baed26428077cef710d
Jeffery Harrell <jefferyharrell@gmail.com>
2023-10-19 10:22:13 -0700

    Tidy up setup_logging, and truncate log file at each app launch

--- NewUI.py
+++ NewUI.py
@@ -9,11 +9,17 @@ if __name__ == "__main__":
     # One-time-initialize PathInfo object. This has to be done inside __main__ so we can use __file__.
     PathInfo(__file__)
 
+    log_path = PathInfo().user_log_folder / "NewUI.log"
+    with log_path.open("w") as file:
+        pass
+
+    config_path = PathInfo().application_folder / "log_config.json"
+
     setup_logging(
         full_context=1,
-        log_path=str(PathInfo().user_log_folder / "NewUI.log"),
+        log_path=str(log_path),
         capture_print=True,
-        config_path=str(PathInfo().application_folder / "log_config.json"),
+        config_path=str(config_path),
     )
 
     app_controller = AppController()
