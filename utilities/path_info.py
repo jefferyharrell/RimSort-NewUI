9ea807aafe3c99cd15eb06b8422f6f89315349e1
Jeffery Harrell <jefferyharrell@gmail.com>
2023-10-19 08:42:20 -0700

    Add docstrings to PathInfo

--- utilities/path_info.py
+++ utilities/path_info.py
@@ -6,17 +6,32 @@ from platformdirs import user_log_dir
 
 class PathInfo:
     """
-    A class that provides information about the paths of files and folders.
+    A singleton class that provides information about the paths of files and folders.
+
+    This class should be initialized once with the __file__ attribute to set the application's root directory.
+    Subsequent accesses to the class will use the already initialized instance.
     """
 
     _instance: Optional["PathInfo"] = None
 
     def __new__(cls, file_path: Optional[str] = None) -> "PathInfo":
+        """
+        Create a new instance or return the existing one.
+
+        :param file_path: The path to the current file, typically __file__.
+        :return: An instance of PathInfo.
+        """
         if not cls._instance:
             cls._instance = super(PathInfo, cls).__new__(cls)
         return cls._instance
 
     def __init__(self, file_path: Optional[str] = None) -> None:
+        """
+        Initialize the PathInfo instance.
+
+        :param file_path: The path to the current file, typically __file__.
+        :raises ValueError: If file_path is not provided.
+        """
         if hasattr(self, "_is_initialized") and self._is_initialized:
             return
 
