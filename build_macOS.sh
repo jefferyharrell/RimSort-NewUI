8eaacfabe4db3a6b0deae8efc115f6b7b16c199a
Jeffery Harrell <jefferyharrell@gmail.com>
2023-10-20 17:10:19 -0700

    Make macOS build script easier to read

--- build_macOS.sh
+++ build_macOS.sh
@@ -1,1 +1,10 @@
-python -m nuitka --standalone --enable-plugin=pyside6 --macos-create-app-bundle --macos-app-icon=resources/AppIcon_a.png --output-dir=./build --include-data-file=./log_config.json=log_config.json --disable-console NewUI.py
+python -m nuitka \
+    --standalone \
+    --enable-plugin=pyside6 \
+    --macos-create-app-bundle \
+    --macos-app-icon=./resources/AppIcon_a.png \
+    --output-dir=./build \
+    --include-data-file=./log_config.json=log_config.json \
+    --include-data-dir=./resources=resources \
+    --disable-console \
+    NewUI.py
