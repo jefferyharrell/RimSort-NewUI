1c5bfee09e3dae47ff3fb305e7b749146de3ffdf
Jeffery Harrell <jefferyharrell@gmail.com>
2023-10-21 15:11:39 -0700

    Make widgets into instance variables in About dialog

--- views/about_dialog.py
+++ views/about_dialog.py
@@ -4,6 +4,7 @@ from PySide6.QtCore import Qt, QSize, Signal
 from PySide6.QtGui import QPixmap, QKeyEvent
 from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout
 
+from utilities.gui_info import GUIInfo
 from utilities.system_info import SystemInfo
 
 
@@ -16,41 +17,38 @@ class AboutDialog(QWidget):
         if SystemInfo().operating_system != SystemInfo.OperatingSystem.MACOS:
             self.setWindowTitle("About NewUI")
 
-        image_label = QLabel()
+        self.app_icon_label = QLabel()
         pixmap = QPixmap("resources/AppIcon_a.png").scaled(
             QSize(64, 64),
             Qt.AspectRatioMode.KeepAspectRatio,
             Qt.TransformationMode.SmoothTransformation,
         )
-        image_label.setPixmap(pixmap)
-        image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.app_icon_label.setPixmap(pixmap)
+        self.app_icon_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        label = QLabel("NewUI")
+        self.app_name_label = QLabel("NewUI")
 
-        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        font = label.font()
-        font.setBold(True)
-        label.setFont(font)
+        self.app_name_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.app_name_label.setFont(GUIInfo().emphasis_font)
 
-        # Create a QLabel for the copyright notice
-        more_info_label = QLabel(
+        self.more_info_label = QLabel(
             "Version so-n-so whatever.0.0.0\n"
             "© 2023 blah blah blah\n"
             "All rights reserved."
         )
-        more_info_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.more_info_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         # Set the font to a smaller size
-        small_font = more_info_label.font()
+        small_font = self.more_info_label.font()
         small_font.setPointSize(small_font.pointSize() - 1)
-        more_info_label.setFont(small_font)
+        self.more_info_label.setFont(small_font)
 
         layout = QVBoxLayout(self)
         layout.addStretch(1)
-        layout.addWidget(image_label)
-        layout.addWidget(label)
+        layout.addWidget(self.app_icon_label)
+        layout.addWidget(self.app_name_label)
         layout.addStretch(1)
-        layout.addWidget(more_info_label)
+        layout.addWidget(self.more_info_label)
 
         self.setFixedSize(300, 200)
 
