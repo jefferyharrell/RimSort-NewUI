9a82087a7a77fbc2f4c0ffc164fc9cdc83c22d1d
Jeffery Harrell <jefferyharrell@gmail.com>
2023-10-21 18:21:40 -0700

    Handle ↑ and ↓ keypresses in DragDropListView

--- widgets/drag_drop_list_view.py
+++ widgets/drag_drop_list_view.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from PySide6.QtGui import QDragMoveEvent, QDropEvent
+from PySide6.QtGui import QDragMoveEvent, QDropEvent, QKeyEvent
 from PySide6.QtWidgets import QListView, QAbstractItemView, QWidget
 from PySide6.QtCore import Qt
 
@@ -35,3 +35,30 @@ class DragDropListView(QListView):
     def dropEvent(self, event: QDropEvent) -> None:
         # Call the base class's dropEvent method to ensure default behavior is executed
         super().dropEvent(event)
+
+    def keyPressEvent(self, event: QKeyEvent) -> None:
+        # Get the current selection
+        current_index = self.currentIndex()
+
+        # Check for the down arrow key
+        if event.key() == Qt.Key.Key_Down:
+            # Get the next index
+            next_index = self.model().index(
+                current_index.row() + 1, current_index.column()
+            )
+            if next_index.isValid():
+                self.setCurrentIndex(next_index)
+                return
+
+        # Check for the up arrow key
+        elif event.key() == Qt.Key.Key_Up:
+            # Get the previous index
+            previous_index = self.model().index(
+                current_index.row() - 1, current_index.column()
+            )
+            if previous_index.isValid():
+                self.setCurrentIndex(previous_index)
+                return
+
+        # Call the base class's keyPressEvent for other keys
+        super().keyPressEvent(event)
